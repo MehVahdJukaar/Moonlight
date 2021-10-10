5fb42cfac02e9051f4c4425ac629dfa4ff64e5f6
gohkenytp <34868610+gohkenytp@users.noreply.github.com>
2021-10-10 15:40:51 -0400

    Update MimicBlockTile.java

--- src/main/java/net/mehvahdjukaar/selene/blocks/MimicBlockTile.java
+++ src/main/java/net/mehvahdjukaar/selene/blocks/MimicBlockTile.java
@@ -1,29 +1,32 @@
 package net.mehvahdjukaar.selene.blocks;
 
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
-import net.minecraft.nbt.CompoundNBT;
-import net.minecraft.nbt.NBTUtil;
-import net.minecraft.network.NetworkManager;
-import net.minecraft.network.play.server.SUpdateTileEntityPacket;
-import net.minecraft.tileentity.TileEntity;
-import net.minecraft.tileentity.TileEntityType;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.NbtUtils;
+import net.minecraft.network.Connection;
+import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
+import net.minecraft.world.CompoundContainer;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.RandomizableContainerBlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.client.model.ModelDataManager;
 import net.minecraftforge.client.model.data.IModelData;
 import net.minecraftforge.client.model.data.ModelDataMap;
 import net.minecraftforge.client.model.data.ModelProperty;
 import net.minecraftforge.common.util.Constants;
 
+import javax.annotation.Nonnull;
 import java.util.Objects;
 
 
-public abstract class MimicBlockTile extends TileEntity implements IBlockHolder {
+public abstract class MimicBlockTile extends BlockEntity {
 
     public BlockState mimic = Blocks.AIR.defaultBlockState();
     public static final ModelProperty<BlockState> MIMIC = new ModelProperty<>();
 
 
-    public MimicBlockTile(TileEntityType<?> type) {
+    public MimicBlockTile(BlockEntityType<?> type) {
         super(type);
     }
 
@@ -46,25 +49,25 @@ public abstract class MimicBlockTile extends TileEntity implements IBlockHolder 
         return true;
     }
 
-    @Override
-    public void load(BlockState state, CompoundNBT compound) {
-        super.load(state, compound);
-        this.mimic = NBTUtil.readBlockState(compound.getCompound("Mimic"));
-    }
 
     @Override
-    public CompoundNBT save(CompoundNBT compound) {
+    public void load(BlockState state, CompoundTag compound) {
+        super.load(state, CompoundTag compound);
+        this.mimic = NbtUtils.readBlockState(compound.getCompound("Mimic"));
+    }
+
+
+    public CompoundContainer save(CompoundContainer compound) {
         super.save(compound);
-        compound.put("Mimic", NBTUtil.writeBlockState(mimic));
+        compound.put("Mimic", NbtUtils.writeBlockState(mimic));
 
         return compound;
     }
 
     //client
-    @Override
-    public void onDataPacket(NetworkManager net, SUpdateTileEntityPacket pkt) {
+    public void onDataPacket(Connection net, ClientboundBlockEntityDataPacket pkt) {
         BlockState oldMimic = this.mimic;
-        CompoundNBT tag = pkt.getTag();
+        CompoundTag tag = pkt.getTag();
         handleUpdateTag(this.getBlockState(), tag);
         if (!Objects.equals(oldMimic, this.mimic)) {
             ModelDataManager.requestModelDataRefresh(this);
@@ -77,15 +80,15 @@ public abstract class MimicBlockTile extends TileEntity implements IBlockHolder 
     // easiest to implement them based on getUpdateTag()/handleUpdateTag()
 
     @Override
-    public SUpdateTileEntityPacket getUpdatePacket() {
-        return new SUpdateTileEntityPacket(this.worldPosition, 0, this.getUpdateTag());
+    public ClientboundBlockEntityDataPacket getUpdatePacket() {
+        return new ClientboundBlockEntityDataPacket(this.worldPosition, 0, this.getUpdateTag());
     }
 
     // The getUpdateTag()/handleUpdateTag() pair is called whenever the client receives a new chunk
     // it hasn't seen before. i.e. the chunk is loaded
 
     @Override
-    public CompoundNBT getUpdateTag() {
-        return this.save(new CompoundNBT());
+    public CompoundTag getUpdateTag() {
+        return this.save(new CompoundTag());
     }
 }
