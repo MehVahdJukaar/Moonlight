01abaf72808601b376c5b2536ae8e32fafbb07ed
MehVahdJukaar <matteogozzi1998@gmail.com>
2024-05-28 16:46:47 +0200

    quark magnet stuff

--- common/src/main/java/net/mehvahdjukaar/moonlight/core/mixins/compat/PistonReactQuarkMixin.java
+++ common/src/main/java/net/mehvahdjukaar/moonlight/core/mixins/compat/PistonReactQuarkMixin.java
@@ -5,7 +5,7 @@ import net.mehvahdjukaar.moonlight.api.misc.OptionalMixin;
 import org.spongepowered.asm.mixin.Mixin;
 import org.violetmoon.quark.api.IMagnetMoveAction;
 
-@OptionalMixin(classLoaded = true, value = "org.violetmoon.quark.api.IMagnetMoveAction")
+@OptionalMixin(value = "org.violetmoon.quark.api.IMagnetMoveAction")
 @Mixin(IPistonMotionReact.class)
 public interface PistonReactQuarkMixin extends IMagnetMoveAction {
 }
