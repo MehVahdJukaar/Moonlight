8064352935b4edfd29cde32d2a97e10d9e3a98b5
MehVahdJukaar <matteogozzi1998@gmail.com>
2024-08-21 21:03:30 +0200

    geom

--- common/src/main/java/net/mehvahdjukaar/moonlight/api/client/model/CustomGeometry.java
+++ common/src/main/java/net/mehvahdjukaar/moonlight/api/client/model/CustomGeometry.java
@@ -1,6 +1,7 @@
 package net.mehvahdjukaar.moonlight.api.client.model;
 
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.client.resources.model.Material;
 import net.minecraft.client.resources.model.ModelBaker;
 import net.minecraft.client.resources.model.ModelState;
@@ -11,6 +12,6 @@ import java.util.function.Function;
 @FunctionalInterface
 public interface CustomGeometry {
 
-    CustomBakedModel bake(ModelBaker modelBaker, Function<Material, TextureAtlasSprite> spriteGetter, ModelState transform);
+    BakedModel bake(ModelBaker modelBaker, Function<Material, TextureAtlasSprite> spriteGetter, ModelState transform);
 
 }
