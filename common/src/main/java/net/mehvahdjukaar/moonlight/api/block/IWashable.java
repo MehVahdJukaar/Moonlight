449ba13f8ea4b9e83d752c71d15eb543a54445ff
MehVahdJukaar <matteogozzi1998@gmail.com>
2024-08-26 16:15:07 +0200

    washable stuff

--- common/src/main/java/net/mehvahdjukaar/moonlight/api/block/IWashable.java
+++ common/src/main/java/net/mehvahdjukaar/moonlight/api/block/IWashable.java
@@ -3,8 +3,16 @@ package net.mehvahdjukaar.moonlight.api.block;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.Vec3;
 
 public interface IWashable {
 
-    boolean tryWash(Level level, BlockPos pos, BlockState state);
+    @Deprecated(forRemoval = true)
+    default boolean tryWash(Level level, BlockPos pos, BlockState state) {
+        return false;
+    }
+
+    default boolean tryWash(Level level, BlockPos pos, BlockState state, Vec3 hitPos) {
+        return this.tryWash(level, pos, state);
+    }
 }
