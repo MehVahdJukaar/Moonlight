614f46d06782ddc2a9c993505e989d68649a74d0
MehVahdJukaar <matteogozzi1998@gmail.com>
2025-03-31 13:50:42 +0200

    fixed rescale

--- common/src/main/java/net/mehvahdjukaar/moonlight/core/mixins/MapDataMixin.java
+++ common/src/main/java/net/mehvahdjukaar/moonlight/core/mixins/MapDataMixin.java
@@ -14,6 +14,7 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.ai.behavior.BehaviorUtils;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockGetter;
@@ -216,6 +217,7 @@ public abstract class MapDataMixin extends SavedData implements ExpandedMapData 
                     customData.save(t);
                     ed.getCustomData().get(entry.getKey()).load(t);
                 }
+                BehaviorUtils.lockGazeAndWalkToEachOther();
             }
         }
     }
