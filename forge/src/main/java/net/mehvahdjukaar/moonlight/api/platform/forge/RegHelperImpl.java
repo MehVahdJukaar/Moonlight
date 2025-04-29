6a3502a1e857ea23b293f3787b5fa54ae81498ed
MehVahdJukaar <matteogozzi1998@gmail.com>
2025-04-29 04:05:20 +0200

    forge bs

--- forge/src/main/java/net/mehvahdjukaar/moonlight/api/platform/forge/RegHelperImpl.java
+++ forge/src/main/java/net/mehvahdjukaar/moonlight/api/platform/forge/RegHelperImpl.java
@@ -58,10 +58,7 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.javafmlmod.FMLModContainer;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegisterEvent;
-import net.minecraftforge.registries.RegistryObject;
+import net.minecraftforge.registries.*;
 
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
