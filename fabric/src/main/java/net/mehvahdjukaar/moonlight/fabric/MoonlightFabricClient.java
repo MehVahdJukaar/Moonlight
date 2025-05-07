cd7e058d7bb747b104c85842c2e8c216e4388b24
MehVahdJukaar <matteogozzi1998@gmail.com>
2025-05-07 14:44:32 +0200

    particle stuff

--- fabric/src/main/java/net/mehvahdjukaar/moonlight/fabric/MoonlightFabricClient.java
+++ fabric/src/main/java/net/mehvahdjukaar/moonlight/fabric/MoonlightFabricClient.java
@@ -1,7 +1,12 @@
 package net.mehvahdjukaar.moonlight.fabric;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.mehvahdjukaar.moonlight.core.client.MLRenderTypes;
+import net.mehvahdjukaar.moonlight.core.mixins.fabric.ParticleEngineAccessor;
+import net.minecraft.client.particle.ParticleRenderType;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
@@ -18,6 +23,10 @@ public class MoonlightFabricClient implements ClientModInitializer {
         CLIENT_SETUP_WORK.forEach(Runnable::run);
         PRE_CLIENT_SETUP_WORK.clear();
         CLIENT_SETUP_WORK.clear();
+
+        List<ParticleRenderType> renderOrder = new ArrayList<>(ParticleEngineAccessor.getRENDER_ORDER());
+        renderOrder.add(MLRenderTypes.PARTICLE_ADDITIVE_TRANSLUCENCY_RENDER_TYPE);
+        ParticleEngineAccessor.setRENDER_ORDER(renderOrder);
     }
 
     public static Queue<Runnable> CLIENT_SETUP_WORK = new ConcurrentLinkedQueue<>();
