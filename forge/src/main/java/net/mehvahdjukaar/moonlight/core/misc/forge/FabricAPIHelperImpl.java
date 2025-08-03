e00b59f82156020cad94552331867976a15a40ff
MehVahdJukaar <matteogozzi1998@gmail.com>
2025-08-03 12:46:25 +0200

    dumb fabric on forge mega hack

--- forge/src/main/java/net/mehvahdjukaar/moonlight/core/misc/forge/FabricAPIHelperImpl.java
+++ forge/src/main/java/net/mehvahdjukaar/moonlight/core/misc/forge/FabricAPIHelperImpl.java
@@ -2,7 +2,30 @@ package net.mehvahdjukaar.moonlight.core.misc.forge;
 
 import net.minecraft.core.RegistryAccess;
 
+import java.lang.reflect.Field;
+
 public class FabricAPIHelperImpl {
-    public static void assignDumbStaticThreadLocal(RegistryAccess.Frozen a) {
+
+    private static final Field FIELD;
+
+    static{
+        Field f = null;
+        try {
+            var clazz =Class.forName("net.fabricmc.fabric.impl.resource.conditions.ResourceConditionsImpl");
+            f = clazz.getDeclaredField("CURRENT_REGISTRIES");
+            f.setAccessible(true);
+        }catch (Exception ignored){
+        }
+        FIELD = f;
+    }
+
+    public static void assignDumbStaticThreadLocal(RegistryAccess.Frozen registryAccess) {
+        //WHY the fuck do we have to fix FABRIC ON FORGE!!!
+        try{
+            FIELD.set(null, registryAccess);
+        }catch (Exception ignored){
+
+        }
+
     }
 }
