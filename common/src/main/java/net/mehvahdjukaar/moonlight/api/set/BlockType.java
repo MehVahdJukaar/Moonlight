6f05d8f2dd63ea9b28d61d9dcf1d8c692609c1c8
Xel'Bayria <12866666+xelbayria@users.noreply.github.com>
2025-08-22 11:41:41 +0200

    BlockType: Improved the logic of detecting Children

--- common/src/main/java/net/mehvahdjukaar/moonlight/api/set/BlockType.java
+++ common/src/main/java/net/mehvahdjukaar/moonlight/api/set/BlockType.java
@@ -5,7 +5,6 @@ import com.google.common.collect.HashBiMap;
 import net.mehvahdjukaar.moonlight.api.resources.assets.LangBuilder;
 import net.mehvahdjukaar.moonlight.api.util.INamedSupplier;
 import net.mehvahdjukaar.moonlight.api.util.Utils;
-import net.mehvahdjukaar.moonlight.core.CommonConfigs;
 import net.mehvahdjukaar.moonlight.core.Moonlight;
 import net.mehvahdjukaar.moonlight.core.set.BlockSetInternal;
 import net.minecraft.core.Registry;
@@ -159,16 +158,18 @@ public abstract class BlockType {
 
 
     @Nullable
-    protected <V> V findRelatedEntry(String after, Registry<V> reg) {
-        return findRelatedEntry(after, "", reg);
+    protected <V> V findRelatedEntry(String prefixOrInfix, Registry<V> reg) {
+        return findRelatedEntry(prefixOrInfix, "", reg);
     }
 
     @Nullable
-    protected <V> V findRelatedEntry(String before, String after, Registry<V> reg) {
-        if (!after.isEmpty()) after = "_" + after;
+    protected <V> V findRelatedEntry(String prefixOrInfix, String suffix, Registry<V> reg) {
+        String prefixed = (prefixOrInfix.isEmpty()) ? "" : prefixOrInfix + "_";
+        String infixed = (prefixOrInfix.isEmpty()) ? "" : "_" + prefixOrInfix;
+        String suffixed = (suffix.isEmpty()) ? "" : "_" + suffix;
         ResourceLocation[] targets = {
-                id.withPath(id.getPath() + "_" + before + after),
-                id.withPath(before + "_" + id.getPath() + after),
+                id.withPath(id.getPath() + infixed + suffixed),
+                id.withPath(prefixed + id.getPath() + suffixed),
         };
         return Utils.findFirstInRegistry(reg, targets);
     }
