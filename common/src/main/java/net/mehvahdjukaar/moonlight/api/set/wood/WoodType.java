d2b0443f062fa94c3924a3757c87a22e1a400442
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2025-09-10 02:11:46 -0600

    WoodType: Fixed the detection in makeKnownIDConventionsAffix

--- common/src/main/java/net/mehvahdjukaar/moonlight/api/set/wood/WoodType.java
+++ common/src/main/java/net/mehvahdjukaar/moonlight/api/set/wood/WoodType.java
@@ -286,18 +286,15 @@ public class WoodType extends BlockType {
         boolean noneEmpty = !prefixOrInfix.isEmpty() && !suffix.isEmpty();
         String prefix_ = prefixOrInfix.isEmpty() ? "" : prefixOrInfix + "_";
         String _infix = prefixOrInfix.isEmpty() ? "" : "_" + prefixOrInfix;
-        String _suffix = suffix.isEmpty() ? "" : "_" + prefixOrInfix;
+        String _suffix = suffix.isEmpty() ? "" : "_" + suffix;
 
         List<ResourceLocation> targets = new ArrayList<>();
         targets.add(ResourceLocation.fromNamespaceAndPath(myNamespace, myPath + _infix + _suffix));
-        targets.add(ResourceLocation.fromNamespaceAndPath(myNamespace, prefix_ + myPath));
+        targets.add(ResourceLocation.fromNamespaceAndPath(myNamespace, prefix_ + myPath + _suffix));
         if (alternateNamespace != null)
             targets.add(ResourceLocation.fromNamespaceAndPath(myNamespace, alternateNamespace + _infix + _suffix));
-        if (noneEmpty) {
-            targets.add(ResourceLocation.fromNamespaceAndPath(myNamespace, prefix_ + myPath + _suffix));
-            if (alternateNamespace != null)
-                targets.add(ResourceLocation.fromNamespaceAndPath(myNamespace, prefix_ + alternateNamespace + _suffix));
-        }
+        if (noneEmpty && alternateNamespace != null)
+            targets.add(ResourceLocation.fromNamespaceAndPath(myNamespace, prefix_ + alternateNamespace + _suffix));
 
         //For things like grimwood_wood -> grimwood
         if (myPath.endsWith(suffix)) {
