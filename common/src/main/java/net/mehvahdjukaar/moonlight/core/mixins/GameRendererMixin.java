ae9303bced814ecbd5e935da8ae4fdf870bda118
decce6 <decce6@proton.me>
2025-12-27 09:11:19 +0800

    Fix fixShade option, closes #408

--- common/src/main/java/net/mehvahdjukaar/moonlight/core/mixins/GameRendererMixin.java
+++ common/src/main/java/net/mehvahdjukaar/moonlight/core/mixins/GameRendererMixin.java
@@ -10,8 +10,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(value = GameRenderer.class, priority = 501)
 public abstract class GameRendererMixin {
 
-    @Inject(method = "render", at = @At(value = "NEW",
-            target = "(Lnet/minecraft/client/Minecraft;Lnet/minecraft/client/renderer/MultiBufferSource$BufferSource;)Lnet/minecraft/client/gui/GuiGraphics;"))
+    @Inject(method = "render", at = @At(value = "INVOKE",
+            target = "Lcom/mojang/blaze3d/platform/Lighting;setupFor3DItems()V"))
     private void moonlight$messWithEntityRendererLighting(float partialTicks, long nanoTime, boolean renderLevel, CallbackInfo ci) {
         MoonlightClient.beforeRenderGui();
     }
