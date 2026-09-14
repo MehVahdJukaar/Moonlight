d75a74319a53098072bc68e675a9277dbf259643
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2026-09-13 22:39:21 -0600

    HardcodedBlockTypes: Updated regions_unexplored:bamboo's WoodType
    (cherry picked from commit 68d99d0cfbe02c28f72d6c1c14d654a8795934c6)

--- common/src/main/java/net/mehvahdjukaar/moonlight/api/integration/HardcodedBlockTypes.java
+++ common/src/main/java/net/mehvahdjukaar/moonlight/api/integration/HardcodedBlockTypes.java
@@ -404,7 +404,7 @@ public class HardcodedBlockTypes {
                 .equivalentWood("pokecube_legends:aged");
 
         // REGIONS UNEXPLORED - REASON: Associated WoodType
-        leafReg.addLeavesToWoodMapping("regions_unexplored:bamboo", "minecraft:jungle");
+        leafReg.addLeavesToWoodMapping("regions_unexplored", "bamboo", "bamboo");
 
         // THE TWILIGHT FOREST - REASON: Associated WoodType, Naming-Convention
         leafReg.addLeavesToWoodMapping("twilightforest", "beanstalk", "twilight_oak");
