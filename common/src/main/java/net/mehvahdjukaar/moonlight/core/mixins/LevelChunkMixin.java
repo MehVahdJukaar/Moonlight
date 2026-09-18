8ee80d15ae2a2e2c0c9f84a2285a3a2a68f7206a
MehVahdJukaar <matteogozzi1998@gmail.com>
2026-09-18 19:42:08 +0200

    better be mixin

--- common/src/main/java/net/mehvahdjukaar/moonlight/core/mixins/LevelChunkMixin.java
+++ common/src/main/java/net/mehvahdjukaar/moonlight/core/mixins/LevelChunkMixin.java
@@ -1,16 +1,35 @@
 package net.mehvahdjukaar.moonlight.core.mixins;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.chunk.LevelChunk;
 import org.slf4j.Logger;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(LevelChunk.class)
 public abstract class LevelChunkMixin {
+
+    @Shadow
+    public abstract void removeBlockEntity(BlockPos pos);
+
+    // vanilla only clears the block entity when the block changes, not when a state without one is set
+    @ModifyExpressionValue(method = "setBlockState",
+            at = @At(value = "INVOKE",
+                    ordinal = 1,
+                    target = "Lnet/minecraft/world/level/block/state/BlockState;is(Lnet/minecraft/world/level/block/Block;)Z"))
+    private boolean moonlight$cleanupOptionalBlockEntity(boolean original, BlockPos pos, BlockState state, boolean isMoving,
+                                                        @Local(ordinal = 1) BlockState oldState) {
+        if (oldState.is(state.getBlock()) && oldState.hasBlockEntity() && !state.hasBlockEntity()) {
+            this.removeBlockEntity(pos);
+        }
+        return original;
+    }
 
     //TODO: remove from IW
     //fixing a vanilla bug that causes log spam when a block that can provide a tile doesn't actually provide it (they can do this now)
